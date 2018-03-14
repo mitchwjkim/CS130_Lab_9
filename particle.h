25f3e59fc3f7c22acd42a60995dad050cacec7d0
Mitchell Kim <mkim106@wch132-30.cs.ucr.edu>
2018-03-14 11:07:58 -0700

    Finished Lab 09

--- particle.h
+++ particle.h
@@ -16,7 +16,14 @@ struct Particle {
 		force = vec3(0,0,0);
 	}
 	void Handle_Collision(float damping, float coeff_resititution) {
-		
+		if(pos[1] < 0) {
+			pos[1] = 0;
+			if(velo[1] < 0) {
+				velo[1] = -1.0 * coeff_resititution * velo[1];
+				velo[1] *= damping;
+				velo[2] *= damping;
+			}
+		}
 	}
 };
 
